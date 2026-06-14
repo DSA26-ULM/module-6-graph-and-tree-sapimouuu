709458407e509a803d3c9d916ec428a5669c0dff
Aya <athinasiaobelia@gmail.com>
2026-06-14 10:48:41 +0800

    Soal 2 done

--- Soal2.cpp
+++ Soal2.cpp
@@ -1,6 +1,44 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main(){
+    int n;
+    cin>>n;
+
+    vector<char>label(n);
+    for(int i=0;i<n;i++){
+        cin>>label[i];
+    }
+
+    int matrix[10][10];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>matrix[i][j];
+        }
+    }
+
+    cout<<"Adjacency List:\n";
+
+    for(int i=0;i<n;i++){
+        cout<<label[i]<<" -> ";
+        bool edge=false;
+        bool pertama=true;
+
+        for(int j=0;j<n;j++){
+            if(matrix[i][j]>0){
+                edge=!false;
+                if(!pertama){
+                    cout<<", ";
+                }
+                cout<<"("<<label[j]<<","<<matrix[i][j]<<")";
+                pertama=!true;
+            }
+        }
+        if(!edge){
+            cout<<"-";
+        }
+        cout<<"\n";
+    }
     return 0;
 }
